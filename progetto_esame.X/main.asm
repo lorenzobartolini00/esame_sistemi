a7ab0786ccdde80c821ba88c35ba43f2fa084747
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-07-10 23:25:01 +0200

    Aggiunto lampeggio led
    Durata lampeggio 1 secondo. Prossimo step: sostituire il lampeggio con l'incremento di una variabile che rappresenta i secondi da inviare tramite porta seriale.

--- progetto_esame.X/main.asm
+++ progetto_esame.X/main.asm
@@ -77,6 +77,10 @@ start
 		bcf INTCON, RBIF
 		bsf INTCON, RBIE
 		
+		;abilito interrupt da timer 1
+		banksel PIE1
+		bsf PIE1, TMR1IE
+		
 		;inizializzo portb_prev
 		movlw	0x01    ; Pulsante non premuto -> switch aperto -> Vdd -> Logic 1
 		movwf	portb_prev
@@ -86,8 +90,21 @@ start
 		movlw	B'00000101'
 		movwf	PORTD
 		
+		;ricarico il timer
+		pagesel reload_tmr1
+		call	reload_tmr1
+		
+		;faccio partire il cronometro
+		banksel T1CON
+		bsf	T1CON, TMR1ON
+		
+		;Abilito interrupt per timer1
+		banksel PIE1
+		bsf     PIE1, TMR1IE
+		
 		;abilito gloabal interrupt enable
 		bsf INTCON, GIE
+		bsf INTCON, PEIE
 main_loop
 		
 wait_sleep
@@ -150,11 +167,6 @@ test_timer0
 		;setto can_sleep
 		bsf flags, CAN_SLEEP
 		
-		;toggle led di debug
-		movlw   0x08        ;Corrisponde alla costante 00000001
-		pagesel toggle_led
-		call toggle_led
-		
 		goto irq_end
 test_button
 		btfss	INTCON, RBIF
@@ -195,18 +207,41 @@ test_button
 		pagesel toggle_led
 		call toggle_led
 		
+		;faccio il toggle del cronometro
+		;banksel T1CON
+		;movlw	0x01
+		;xorwf   T1CON, f
+		
 button_end
 		; salva nuovo stato di PORTB su portb_prev
 		banksel PORTB
 		movf	PORTB, w
 		movwf	portb_prev
 
-		goto	irq_end		
-
+		goto	irq_end	
 		
 test_timer1
+		;controllo se il timer 1 è scaduto
+		banksel	PIE1
+		btfss	PIE1, TMR1IE
+		goto irq_end
+		banksel	PIR1
+		btfss	PIR1, TMR1IF
+		goto irq_end
+		
+		;ricarico il timer
+		pagesel reload_tmr1
+		call	reload_tmr1
+		
+		;toggle led timer
+		movlw   0x02        ;Corrisponde alla costante 00000010
+		pagesel toggle_led
+		call toggle_led
+		
+		goto irq_end
 		
 test_usart
+		
 		
 irq_end
 		;ripristino del contesto
@@ -235,6 +270,25 @@ toggle_led
 		
 		return
 		
+reload_tmr1
+    
+	;Carico la costante sul registro a 16 bit
+	banksel	TMR1L
+	movlw	low  tmr_1s
+	movwf	TMR1L
+	movlw	high tmr_1s
+	movwf	TMR1H
+
+	;Azzero il bit di flag
+	banksel	    PIR1
+	bcf	    PIR1, TMR1IF
+
+	;Faccio partire il timer
+	banksel T1CON
+	bsf     T1CON, TMR1ON
+
+	return
+		
 ;-----------------------------------------------------------------------------------------------
 		
 initHw
@@ -256,7 +310,9 @@ initHw
 	;il pulsante che vogliamo utilizzare è RB0
 	setRegK	TRISB, 0xFF
 	;setto come input anche PORTA e PORTE
+	movlw	0xFF
 	movwf	TRISA
+	movwf	TRISC
 	movwf	TRISE
 	
 	;disattivo input analogico su tutti i pin di PORTB
@@ -286,10 +342,10 @@ initHw
 	;TMR1GE = 0 -> gate disabilitato
 	;T1CKPS<1:0> = 00 -> prescaler a 1:1
 	;T1OSCEN = 1 -> oscillatore LP abilitato
-	;#T1SYNC = 0 -> sincronizzazione disabilitata(il timer deve lavorare anche in sleep)
+	;#T1SYNC = 1 -> sincronizzazione disabilitata(il timer deve lavorare anche in sleep)
 	;TMR1CS = 1 -> seleziono sorgente esterna, oscillatore LP, dato che quando il micro è in sleep il timer deve continuare a contare
 	;TMR1ON = 0 -> timer è disattivato, verrà attivato al momento opportuno
-	setRegK T1CON, B'00001010'
+	setRegK T1CON, B'00001110'
 	
 	
 ;Configuro modulo EUSART
